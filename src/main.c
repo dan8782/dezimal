0327d83711c15fa0601617a5ea100796f95dcf60
Danila Kiva <kivadan@inbox.ru>
2023-02-12 15:39:58 +0300

    feat:added negate

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 typedef struct 
 {
@@ -7,18 +8,27 @@ typedef struct
 } s21_decimal;
 
 void print_dec(s21_decimal decl);
+void s21_copy(s21_decimal *decimal, s21_decimal origdec);
+void set_sign(s21_decimal *decl, bool sign);
 int get_bit(s21_decimal decimal, int index);
 int get_sign(s21_decimal decl);
-s21_decimal *set_bit(s21_decimal *decimal, int index);
+int s21_negate(s21_decimal value, s21_decimal *result);
+s21_decimal *set_bit(s21_decimal *decimal, int index,bool sign);
 s21_decimal *unset_bit(s21_decimal *decimal, int index);
+s21_decimal *switch_bit(s21_decimal *decimal, int index);
 
 int main(int argc, char const *argv[])
 {
 	s21_decimal decimal = {0,8,16,32};
-	set_bit(&decimal, 127);
-	unset_bit(&decimal, 127);
-	printf("%d\n",get_sign(decimal));
+	s21_decimal newdec;
+	// s21_copy(&newdec,decimal);
+	// set_bit(&decimal, 127,1);
 	print_dec(decimal);
+	printf("\n");
+	s21_negate(decimal,&newdec);
+	// unset_bit(&decimal, 127);
+	// printf("%d\n",get_sign(decimal));
+	print_dec(newdec);
 	return 0;
 }
 
@@ -31,8 +41,13 @@ s21_decimal *unset_bit(s21_decimal *decimal, int index) {
 	return decimal;
 }
 
-s21_decimal *set_bit(s21_decimal *decimal, int index){
-	decimal->bits[index/32] |= (1 << (index%32));	
+s21_decimal *set_bit(s21_decimal *decimal, int index, bool sign){
+	decimal->bits[index/32] |= (sign << (index%32));	
+	return decimal;
+}
+
+s21_decimal *switch_bit(s21_decimal *decimal, int index){
+	decimal->bits[index/32] ^= (1 << (index%32)); //тут XOR 
 	return decimal;
 }
 
@@ -46,7 +61,25 @@ void print_dec(s21_decimal decl) {
   printf("\n");
 }
 
+
+void s21_copy(s21_decimal *decimal, s21_decimal origdec) {
+  for (int i = 0; i < 4; ++i) {
+    decimal->bits[i] = origdec.bits[i];
+  }
+}
+
+int s21_negate(s21_decimal value, s21_decimal *result) {
+
+    s21_copy(result, value);
+	switch_bit(result,127);
+
+    return 0;
+}
+
+void set_sign(s21_decimal *decl, bool sign) {
+  set_bit(decl, 127, sign);
+}
+
 int get_sign(s21_decimal decl) { 
 	return get_bit(decl, 127); 
 }
-
