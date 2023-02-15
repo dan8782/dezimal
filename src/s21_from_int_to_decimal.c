e38072766f7cc67df4e57408a981946d6c03846d
Artem <gizo@MacBook-Air-Artem.local>
2023-02-15 10:25:17 +0300

    edit

--- src/s21_from_int_to_decimal.c
+++ src/s21_from_int_to_decimal.c
@@ -2,18 +2,13 @@
 
 
 int s21_from_int_to_decimal(int src, s21_decimal *dst) {
-   int res = 1;
-  if (dst) {
-    dst->bits[0] = 0;
-    dst->bits[1] = 0;
-    dst->bits[2] = 0;
-    dst->bits[3] = 0;
+   memset(&(dst->bits), 0, sizeof(dst->bits));
     if (src < 0) {
       set_sign(dst, 1);
       src *= -1;
     }
     dst->bits[0] = src;
-    res = 0;
+    return 0;
   }
-  return res;
-}
+  
+// }
