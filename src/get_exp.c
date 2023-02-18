6d3a093b706c5007db169d88508b868b0120a88f
Danila Kiva <kivadan@inbox.ru>
2023-02-19 01:25:12 +0300

    zalupa 228

--- src/get_exp.c
+++ src/get_exp.c
@@ -1,11 +1,11 @@
 #include "s21_decimal.h"
 
-// Получаем степень  112 - старший бит, 119 - младший
-int get_exp(s21_decimal x) {
-  int exp = 0;
-  for (int i = LAST_EXP_BIT; i >= START_EXP_BIT; i--) {
-    exp += get_bit(x, i) * pow(2, LAST_EXP_BIT-i);
-   // printf("i = %d, exp_2 = %d\n", i, LAST_EXP_BIT-i);
+int get_exp(s21_decimal decl) {
+  int scale = 0;
+  for (int i = 119; i >= 112; i--) {
+    scale <<= 1;
+    scale |= get_bit(decl, i);
   }
-  return exp;
+  return scale;
 }
+
---
+++ src/s21_add.c
@@ -0,0 +1,23 @@
+#include "s21_decimal.h"
+
+
+int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result){
+	*result = value_2;
+	return 0;
+}
+
+void set_exp(s21_decimal *decimal,int exp){
+	// for (int i = 112; i < 120; i++)
+	// {
+	// 	set_bit(decimal,i,0);
+	// }
+	// decimal->bits[3] |= exp << 8;
+	// print_dec(*decimal);
+	for (int i = 112; i < 120; i++)
+	{
+		set_bit(decimal, i, exp & 1);
+		exp >>= 1;
+	}
+
+	printf("%d",get_exp(*decimal));
+}
