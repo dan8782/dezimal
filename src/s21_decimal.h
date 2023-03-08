e212a042da9302e7f90b8c89d054423a46fe0cc6
Игорь Грицюк <igor@MacBook-Air-Igor.local>
2023-03-08 23:25:41 +0300

    left_shift

--- src/s21_decimal.h
+++ src/s21_decimal.h
@@ -1,12 +1,11 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
 #include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-typedef struct 
-{
-    int bits[4];
+typedef struct {
+  int bits[4];
 } s21_decimal;
 
 void print_dec(s21_decimal decl);
@@ -16,7 +15,7 @@ int get_bit(s21_decimal decimal, int index);
 int get_sign(s21_decimal decl);
 int s21_negate(s21_decimal value, s21_decimal *result);
 int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
-s21_decimal *set_bit(s21_decimal *decimal, int index,bool sign);
+s21_decimal *set_bit(s21_decimal *decimal, int index, bool sign);
 s21_decimal *unset_bit(s21_decimal *decimal, int index);
 s21_decimal *switch_bit(s21_decimal *decimal, int index);
 int s21_from_int_to_decimal(int src, s21_decimal *dst);
@@ -28,7 +27,8 @@ int s21_is_greater(s21_decimal x, s21_decimal y);
 int s21_is_greater_or_equal(s21_decimal x, s21_decimal y);
 int s21_is_less_or_equal(s21_decimal x, s21_decimal y);
 void test(s21_decimal x, s21_decimal y);
-void set_exp(s21_decimal *decimal,int exp);
+void set_exp(s21_decimal *decimal, int exp);
+int left_shift(s21_decimal *decimal);
 
 #define SIGN_BIT 127
 #define START_EXP_BIT 112
