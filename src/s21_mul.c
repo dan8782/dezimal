e886b694fade6547139b566ac02ce9b1dc0e179b
Kotone Debbra <kotonede@at-r7.msk.21-school.ru>
2023-03-11 16:59:04 +0300

    kkeks

--- src/s21_mul.c
+++ src/s21_mul.c
@@ -1,7 +1,7 @@
 #include "s21_decimal.h"
 
 int addhelp(int a,int b);
-int Bitwise_add(int a,int b);
+int Bitwise_add(s21_decimal value_a,s21_decimal value_b,s21_decimal *result);
 int multiplyTwoNumbers(int a, int b);
 
 int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
@@ -11,19 +11,32 @@ int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
 //   }
 
   printf("\n");
-  result->bits[0] = multiplyTwoNumbers(1, 2147483646);
+  // result->bits[0] = multiplyTwoNumbers(1, 2147483647);
+  Bitwise_add(value_1,value_2, result);
 //   result->bits[1]= safe_shl(result->bits[1],1);
 //   result->bits[1]= result->bits[1]<<1;
   return 0;
 }
 
-int Bitwise_add(int a, int b) {
-    for (int i = 0; i < 96; i++) {
-          int buffer = a & b;
-          a = a ^ b;
-          b = buffer << 1;
-        }
-        return a;
+int Bitwise_add(s21_decimal value_a, s21_decimal value_b, s21_decimal *result) {
+  int temp_bit = result->bits[3];
+  int buffer = 0;
+  int bit_a = 0;
+  int bit_b = 0;
+  s21_decimal bufferd;
+  s21_is_null(&bufferd);
+  for (int i = 0; i < 96; i++) {
+    bit_a = get_bit(value_a, i);
+    bit_b = get_bit(value_b, i);
+    buffer = bit_a & bit_b;
+    set_bit(&bufferd, i, bit_a & bit_b);
+    set_bit(result, i, bit_a ^ bit_b);
+    if (buffer != 0) {
+      left_shift(&bufferd);
+      value_b=bufferd;
+    }
+  }
+  return buffer;
 }
 
 int add_helper(s21_decimal val_1, s21_decimal val_2, s21_decimal *result) {
