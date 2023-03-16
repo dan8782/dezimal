388c267c3e24b69f400d82b4d85a58d179d63401
[KonstantinovK] <[konstantinov87@mail.ru]>
2023-03-16 13:47:57 +0300

    turnc, minor changes

--- src/converters/s21_from_float_to_decimal.c
+++ src/converters/s21_from_float_to_decimal.c
@@ -2,11 +2,9 @@
 #include <math.h>
 
 int s21_from_float_to_decimal(float src, s21_decimal *dst) {
-    int return = 0;
     s21_set_null(dst);
     src = fabsf(src);
 
-   
   if (src < 0) {
     set_sign(dst, 1);
     src *= -1;
