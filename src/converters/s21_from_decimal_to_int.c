382075471dfffd422c217ded176055c4a558b3f7
[KonstantinovK] <[konstantinov87@mail.ru]>
2023-03-16 15:56:10 +0300

    fix dec_to_int

--- src/converters/s21_from_decimal_to_int.c
+++ src/converters/s21_from_decimal_to_int.c
@@ -1,9 +1,11 @@
 #include "../s21_decimal.h"
 
 int s21_from_decimal_to_int(s21_decimal src, int *dst) {
-  if (get_exp(src)) {
+ /*if (get_exp(src)) {
     s21_truncate(src, &src);
   }
+*/
+*dst = 0;
 
   if (src.bits[2] || src.bits[1] || src.bits[0] > 0x7fffffff) {
     return 1;
