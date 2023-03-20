ff5c8d9fe3f248a57e5dc06c3323300dc8154529
Игорь Грицюк <igor@MacBook-Air-Igor.local>
2023-03-20 22:05:44 +0300

    fix sub: res = a - zero = a

--- src/utils/arithmetic/sub_bitwise.c
+++ src/utils/arithmetic/sub_bitwise.c
@@ -10,17 +10,20 @@ int sub_bitwise(s21_decimal x, s21_decimal y, s21_decimal *res) {
     max = y;
     min = x;
   }
-
-  while (!is_zero(&min)) {
-    for (int i = 0; i < 3; i++) {
-      borrow.bits[i] = ~max.bits[i] & min.bits[i];  // int borrow = (~x) & y;
-      max.bits[i] ^= min.bits[i];                   // x = x ^ y;
-    }
-    // y = borrow << 1;
-    left_shift(&borrow);
-    for (int i = 0; i < 3; i++) {
-      min.bits[i] = borrow.bits[i];
-      res->bits[i] = max.bits[i];
+  if (is_zero(&min)) {
+    copy_decimal(res, max);
+  } else {
+    while (!is_zero(&min)) {
+      for (int i = 0; i < 3; i++) {
+        borrow.bits[i] = ~max.bits[i] & min.bits[i];  // int borrow = (~x) & y;
+        max.bits[i] ^= min.bits[i];                   // x = x ^ y;
+      }
+      // y = borrow << 1;
+      left_shift(&borrow);
+      for (int i = 0; i < 3; i++) {
+        min.bits[i] = borrow.bits[i];
+        res->bits[i] = max.bits[i];
+      }
     }
   }
   return 0;
