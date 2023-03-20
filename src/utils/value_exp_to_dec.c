3d8f63bdcd65b1fd8b504f22d91fdff31808406f
Игорь Грицюк <igor@MacBook-Air-Igor.local>
2023-03-21 00:33:50 +0300

    add_bitwise

--- src/utils/value_exp_to_dec.c
+++ src/utils/value_exp_to_dec.c
@@ -7,13 +7,15 @@ int value_exp_to_decimal(s21_decimal *res, int n) {
   s21_decimal ten = {10, 0, 0, 0};
   s21_decimal buffer = {0, 0, 0, 0};
   set_zero(res);
-  char i = 0;
-  while ( i < n * 10) {
-    printf("%d\n", i);
-    add_bitwise(*res, ten, res);
+  int i = 0;
+  while (i < pow(10, n - 2)) {
+    for (int k = 0; k < 10; k++, add_bitwise(*res, ten, res))
+      ;
     i++;
     print_dec(res);
   }
-   
-return 0;
+
+  return 0;
 }
+// 10 * 10 = 10 + 10 + 10 + 10 10 ^ 2 = (10 + 10 + 10 + 10..+ 10) N =
+//               10 10 ^ 3 = (10 + 10 + 10 + 10..+ 10) N = 10
