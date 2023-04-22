8be09ca893b5b6b1c7f17722e2b0f1448a17f422
Brill Paula <brillpau@at-p8.msk.21-school.ru>
2023-04-22 13:14:48 +0300

    test negate

--- src/other/s21_negate.c
+++ src/other/s21_negate.c
@@ -1,8 +1,12 @@
 #include "../s21_decimal.h"
 
 int s21_negate(s21_decimal value, s21_decimal *result) {
-  copy_decimal(result, value);
-  switch_bit(result, 127);
-
-  return 0;
+  int err = 0;
+  if (result != NULL) {
+    copy_decimal(result, value);
+    switch_bit(result, 127);
+  } else {
+    err = 1;
+  }
+  return err;
 }
