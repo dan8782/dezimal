9a8b63c2c85d57e10773888413211e51af85eb66
Игорь Грицюк <igor@MacBook-Air-Igor.local>
2023-05-07 16:47:07 +0300

    dec to int fix

--- src/tests/test_main.c
+++ src/tests/test_main.c
@@ -1,14 +1,24 @@
 #include "./test_main.h"
 
 int main() {
-  Suite *test_suites[] = {
-      suite_from_decimal_to_int(),
-      // suite_from_decimal_to_float(),
-      suite_from_float_to_decimal(), suite_from_int_to_decimal(),
-      suite_is_equal(), suite_is_less(), suite_is_greater(),
-      suite_is_greater_or_equal(), suite_is_less_or_equal(),
-      suite_is_not_equal(), suite_add(), suite_sub(), suite_truncate(),
-      suite_negate(), suite_floor(), suite_round(), suite_div(), suite_mul()};
+  Suite *test_suites[] = {// suite_from_decimal_to_float(),
+                          suite_from_decimal_to_int(),
+                          suite_from_float_to_decimal(),
+                          suite_from_int_to_decimal(),
+                          suite_is_equal(),
+                          suite_is_less(),
+                          suite_is_greater(),
+                          suite_is_greater_or_equal(),
+                          suite_is_less_or_equal(),
+                          suite_is_not_equal(),
+                          suite_add(),
+                          suite_sub(),
+                          suite_truncate(),
+                          suite_negate(),
+                          suite_floor(),
+                          suite_round(),
+                          suite_div(),
+                          suite_mul()};
   SRunner *suite_runner = srunner_create(test_suites[0]);
   srunner_set_fork_status(suite_runner, CK_NOFORK);
 
