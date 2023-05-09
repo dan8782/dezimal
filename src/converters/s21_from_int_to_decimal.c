0a46b3c75cc8589fc58005fd475832c3d4fd1e54
Игорь Грицюк <igor@MacBook-Air-Igor.local>
2023-05-09 16:02:56 +0300

    cover int to dec

--- src/converters/s21_from_int_to_decimal.c
+++ src/converters/s21_from_int_to_decimal.c
@@ -3,15 +3,15 @@
 int s21_from_int_to_decimal(int src, s21_decimal *dst) {
   // заполненение массива указанными символами = 0
   int err = 0;
-  if (dst == NULL) {
-    err = 1;
-  } else {
+  if (dst != NULL) {
     set_zero(dst);
     if (src < 0) {
       set_sign(dst, 1);
       src *= -1;
     }
     dst->bits[0] = src;
+  } else {
+    err = 1;
   }
   return err;
 }
